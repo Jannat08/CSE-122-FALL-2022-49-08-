63a000770bc4ec2bbf3649e6ad6515cd1919b2f8
Jannat08 <109477752+Jannat08@users.noreply.github.com>
2022-07-19 09:34:40 +0600

    Add files via upload

---
+++ Lab 01/1.350_49_8.cpp
@@ -0,0 +1,15 @@
+//codeforce problem no - 4A
+//problem name - Watermelon 
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int weight;
+    cin>>weight;
+    if(weight%2==0 && weight!=0)
+    cout<< "YES";
+    else
+    cout<<"NO";
+    return 0;
+}
