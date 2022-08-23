87c445ffaa6c70aa5ae9ecd472cf1eb783f166b7
Jannatul Ferdous <jannatulferdous0898@gmail.com>
2022-08-23 06:52:49 +0600

    Add files via upload

---
+++ Lab 04/2.350_49_8.cpp
@@ -0,0 +1,13 @@
+//codeforces problem no-281A
+//problem name- Word capitalization 
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+string s; 
+cin>>s; 
+s[0]=toupper(s[0]); 
+cout<<s<<endl; 
+return 0;
+}
